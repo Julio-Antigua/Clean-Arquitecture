287db6ebb56fe5c50d5fd2bf60ea7d8851018a6e
INTECADM\julio.antigua <julio.antigua@intec.edu.do>
2023-03-09 14:50:57 -0400

    - Se creo la carpeta Infrastructure. - - Se creo la interface IEmailService. - - Se modificaron los parametros de GetAsync en la interface IAsyncRepository haciendo referencia a que pueden ser nuleables. - - Se creo la interface IVideoRepository. -

--- CleanArquitecture.Application/Contracts/Persistence/IAsyncRepository.cs
+++ CleanArquitecture.Application/Contracts/Persistence/IAsyncRepository.cs
@@ -12,15 +12,15 @@ namespace CleanArquitecture.Application.Contracts.Persistence
         Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate);
 
         //Metodo para traer una lista e incluir el ordenamiento de los resultados
-        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null,
-                                        Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
-                                        string includeString=null,
+        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>>? predicate = null,
+                                        Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+                                        string? includeString=null,
                                         bool disableTracking=true);
 
         // Metodo para la paginacion
-        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null,
-                                        Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
-                                        List<Expression<Func<T, object>>> includes = null,
+        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>>? predicate = null,
+                                        Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+                                        List<Expression<Func<T, object>>>? includes = null,
                                         bool disableTracking = true);
 
         //Metodo para traer un valor por id
---
+++ CleanArquitecture.Application/Contracts/Persistence/IVideoRepository.cs
@@ -0,0 +1,10 @@
+﻿using CleanArquitecture.Domain;
+
+namespace CleanArquitecture.Application.Contracts.Persistence
+{
+    public interface IVideoRepository: IAsyncRepository<Video>
+    {
+         Task<Video> GetvideoByName(string name);
+         Task<IEnumerable<Video>> GetVideoByUsername(string username);
+    }
+}
