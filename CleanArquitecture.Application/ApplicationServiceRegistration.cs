23061bb6e3d41a94216edffe845ba24718760c95
INTECADM\julio.antigua <julio.antigua@intec.edu.do>
2023-03-14 09:51:15 -0400

    - Se creo la clase ApplicationServiceRegistration para poder crear la inyeccion de dependencia de los diferentes servicios.

---
+++ CleanArquitecture.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,24 @@
+﻿using CleanArquitecture.Application.Behaviours;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace CleanArquitecture.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            //automaticamente  va a leer todas las clases que esten herredando o implementando las interfaces de automapper y las va a inyectar
+            services.AddAutoMapper(Assembly.GetExecutingAssembly()); 
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+
+            return services;
+        }
+    }
+}
