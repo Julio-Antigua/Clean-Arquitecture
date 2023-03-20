85b6ff032ef21a09d1cd235138496296d7667019
INTECADM\julio.antigua <julio.antigua@intec.edu.do>
2023-03-20 10:29:04 -0400

    - Se creo la carpeta errors y dentro de esta se crearon los metods CodeErrorException y CodeErrorResponse. -

---
+++ CleanArchitecture.API/Errors/CodeErrorException.cs
@@ -0,0 +1,13 @@
+﻿using CleanArquitecture.API.Errors;
+
+namespace CleanArquitectureAPI.Errors
+{
+    public class CodeErrorException : CodeErrorResponse
+    {
+        public string? Details { get; set; }
+        public CodeErrorException(int statusCode, string? messsage = null, string? details = null) : base(statusCode, messsage)
+        {
+            Details = details;
+        }
+    }
+}
